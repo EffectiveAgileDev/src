834ea7c0abba4af7e22349debb6f1728f26e0924
Rod Claar <rodclaar@effectiveagiledev.com>
2024-04-10 12:05:01 -0700

    On branch master  Changes to be committed: 	modified:   starter/.gitignore 	new file:   starter/RunOneColor.docx 	modified:   starter/agents.py 	deleted:    starter/init.py 	modified:   starter/main.py 	new file:   starter/poetry.lock 	modified:   starter/tasks.py

---
+++ starter/main.py
@@ -0,0 +1,70 @@
+from crewai import Crew, Process
+from langchain_openai import ChatOpenAI
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Initialize the OpenAI GPT-4 language model
+OpenAIGPT4 = ChatOpenAI(
+    model="gpt-4"
+)
+# 1. Create Agents
+from agents import ClassAutomaticSocialMediaAgents
+from tasks import ClassAutomaticSocialMediaTasks 
+# Setup Agents 
+agents = ClassAutomaticSocialMediaAgents()
+
+posting_manager = agents.posting_manager()
+listing_data_fill = agents.listing_data_fill()
+post_type_picker = agents.post_type_picker()
+title_creator = agents.title_creator()
+
+# 2. Create Tasks
+
+tasks = ClassAutomaticSocialMediaTasks()
+
+manage_social_media_posts = tasks.manage_social_media_posts(
+    agent=posting_manager,
+    class_URL="https://effectiveagiledev.com/Find/tabid/157/Default.aspx"
+)
+
+manage_listing_data_fill = tasks.manage_listing_data_fill(
+    agent=listing_data_fill,
+    class_URL="https://effectiveagiledev.com/Find/tabid/157/Default.aspx"
+)
+
+manage_post_type_picker = tasks.manage_post_type_picker(
+    agent=post_type_picker
+)
+
+manage_title_creator = tasks.manage_title_creator(
+    agent=title_creator
+)
+
+
+# 3. Create Tools
+
+
+
+# Create a new Crew instance
+crew = Crew(
+    agents=[posting_manager,
+            listing_data_fill,
+            post_type_picker,
+            ],
+    tasks=[manage_social_media_posts,
+           manage_listing_data_fill,
+           manage_post_type_picker,
+           manage_title_creator],
+
+    process=Process.hierarchical,
+    manager_llm=OpenAIGPT4
+)
+
+# Kick of the crew
+results = crew.kickoff()
+
+print("Crew usage", crew.usage_metrics)
+
+print("Crew work results:")
+print(results)
